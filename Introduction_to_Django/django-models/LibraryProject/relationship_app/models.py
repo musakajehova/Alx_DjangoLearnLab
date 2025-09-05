dbebf19ce77d01919618d4a6fafa5c975d8550e4
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-05 23:23:55 +0200

    Task 1 change dir

---
+++ Introduction_to_Django/django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Author (models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Book (models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_name')
+
+    def __str__(self):
+        return f"title: {self.title} author: {self.author.name}" 
+
+class Library (models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='genre')
+
+    def __str__(self):
+        return f"lib_name: {self.name} books: {self.books})"
+
+class Librarian (models.Model):
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.library}"
