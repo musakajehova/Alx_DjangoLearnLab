03acbe1d91a139937ab12a0cb602a7443bc545e7
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-06 00:14:33 +0200

    Task 1 fix query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-Book.objects.get(author="1958")
+
+Book.objects.get(author="book_name")
 
 Library.objects.all()
+
+Library.objects.get(name="library_name")
 
 Librarian.objects.get(library="test")
