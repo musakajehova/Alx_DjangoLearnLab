5c9793040917f8c798d63424053f465c16a53da5
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-06 18:49:23 +0200

    Task 2 fix seperate import Library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 # Create your views here.
 from django.http import HttpResponse
