a3a9bfa82a5da5ef86ad6d75726c7e52378b741f
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-07 22:25:08 +0200

    Task 4 fixes urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
     ############################################################################################################
-    path("book/add/", views.add_book, name="add_book"),
-    path("book/edit/<int:pk>/", views.edit_book, name="edit_book"),
+    path("book/add_book/", views.add_book, name="add_book"),
+    path("book/edit_book/<int:pk>/", views.edit_book, name="edit_book"),
     path("book/delete/<int:pk>/", views.delete_book, name="delete_book"),
 ]
