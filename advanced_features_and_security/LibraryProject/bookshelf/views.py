d4f893c1765b5927b29fc6338259085ce9bae8ae
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-14 07:16:30 +0200

    Task 2 creating Example forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import Permission
 from django.contrib.auth.decorators import permission_required
-from .models import Book, CustomUser
-from django.http import HttpRequest
+from .models import Book
+from .forms import ExampleForm
+#from django.http import HttpRequest
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -50,3 +51,13 @@ def can_delete(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+def Example_add_book(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('Book_list')
+    else:
+        form = ExampleForm(instance=form)
+    return render(request, "bookshelf/form_example.html", {"form": form})
