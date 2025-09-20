7d52e3c5a0b071efee2713156c942b954d0679ea
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-20 05:35:01 +0200

    Task 1 changes generics.listCreateApiView to generics.ListApi

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,6 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
