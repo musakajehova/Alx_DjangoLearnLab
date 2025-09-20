9f58680c0ee9bea12540a6ed3ff92608bcc80edf
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-20 05:26:38 +0200

    Building my  First API Endpoint with Django REST Framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=230)
     author = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"title: {self.title} \n author: {self.author}"
