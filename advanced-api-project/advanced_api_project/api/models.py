b2b090a8a36f0c98c1c34e0044b3232e0f1e7076
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-26 20:22:53 +0200

    Step 3: Define Data Models

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    author = models.CharField(max_length=150)
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
