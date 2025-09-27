4412d739e379016d950e58202e5a849138315ca5
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-27 18:28:16 +0200

    changed permission form  AllowAll to isAuthenticatedOrReadOnly

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework import generics
 from .models import Book, Author
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = AllowAny
+    permission_classes = IsAuthenticatedOrReadOnly
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = AllowAny
+    permission_classes = IsAuthenticatedOrReadOnly
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
