c58cdcc7d16730d4c1b21d6dcad7a27db4783ea5
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-27 14:30:40 +0200

    changes the model author to name

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,24 +2,23 @@ from rest_framework import serializers
 from .models import Author, Book
 from django.utils import timezone
 
+class AuthorSerializer(serializers.ModelSerializer):
+    """This is the Author serializer"""
+    
+    class Meta:
+        model = Author
+        fields = ['name']
+
+
 class BookSerializer(serializers.ModelSerializer):
     """This is the Book serializer"""
+    name = AuthorSerializer(many=True, read_only=True)
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['name', 'title', 'publication_year', 'author']
 
     def validate(self, data):
         if data('publication_year') > timezone.now().date():
             raise serializers.ValidationError("Publication Year must be an int that is 4 int")
         return data
-
-class AuthorSerializer(serializers.ModelSerializer):
-    """This is the Author serializer"""
-    name = BookSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = Author
-        fields = ['author', 'name']
-
-
-    
