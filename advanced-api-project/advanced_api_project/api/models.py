bd9181ba3fbc6ad5d3a900b8300fae5a4f7cdaf2
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-27 13:53:20 +0200

    Step 5: Document Your Model and Serializer Setup and Step 6: Implement and Test

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -3,9 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
+    """This model is the Author model"""
     author = models.CharField(max_length=150)
 
+    def __str__(self):
+        return f"Author: {self.author}"
+
 class Book(models.Model):
+    """This is a Book model, the author field is a foreign key from the author model"""
     title = models.CharField(max_length=250)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Title: {self.title} /n Publication Year: {self.publication_year} /n Author: {self.author}"
