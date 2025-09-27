7822c6113eac3d000a4854d51a5e2e744f7c8946
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-09-27 18:25:03 +0200

    changed the urls configeration

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 urlpatterns =[
     path('books/', BookListView.as_view(), name='book-list' ),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail' ),
-    path('create/', BookCreateView.as_view(), name='book-create' ),
-    path('update/<int:pk>/', BookUpdateView.as_view(), name='book-update' ),
-    path('delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete' ),
+    path('books/create/', BookCreateView.as_view(), name='book-create' ),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update' ),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete' ),
 ]
