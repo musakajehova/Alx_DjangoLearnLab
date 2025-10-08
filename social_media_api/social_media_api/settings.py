f1bf579a6e52f21ed1c78e789eb3d218caa6a8df
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-08 21:55:03 +0200

    Add custom user model and configure REST framework authentication settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -126,3 +126,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+############################################################################
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+    ],
+
+}
+############################################################################
