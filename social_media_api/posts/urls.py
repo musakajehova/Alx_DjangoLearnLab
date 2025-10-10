1a4598204889ddd23a6d1d6b04a390d42f291435
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-10-10 18:02:00 +0200

    Add like/unlike endpoints: include LikePostView and UnlikePostView in URL routing

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,4 +1,4 @@
-from .views import CommentView, PostView, FeedListView
+from .views import CommentView, PostView, FeedListView, LikePostView, UnlikePostView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
@@ -11,5 +11,7 @@ urlpatterns =[
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
     ##############################################################
-     path('feed/', FeedListView.as_view(), name='feed'),
+    path('feed/', FeedListView.as_view(), name='feed'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
